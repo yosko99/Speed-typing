6e28d4aa6216ccaede1112b9a0eb62962ed90995
yosko99 <thedriftmaniaka1997@gmail.com>
2022-03-27 17:48:07 +0300

    Init npm packages

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,15 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+function App () {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <main style={{ minHeight: '90vh' }}>
+        <Routes>
+          <Route path='/' element={<h1>hello</h1>}/>
+        </Routes>
+      </main>
+    </Router>
   );
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,32 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import { ReactQueryDevtools } from 'react-query/devtools';
 import reportWebVitals from './reportWebVitals';
+import './styles/bootstrap.min.css';
+import ReactDOM from 'react-dom';
+import React from 'react';
+import App from './App';
+import './index.css';
+// @ts-ignore
+import { loadProgressBar } from 'axios-progress-bar';
+
+loadProgressBar();
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false
+    }
+  }
+});
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <QueryClientProvider client={queryClient}>
+    <React.StrictMode>
+        <App />
+        <ReactQueryDevtools initialIsOpen/>
+    </React.StrictMode>
+  </QueryClientProvider>,
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
